4a821f0837d78c213f03f9fda043158ff3173d71
TKadyear <tamarakadyear@gmail.com>
2022-01-28 10:38:06 +0100

    Trying Components, Button Counter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue.js Components Fundamentals</h1>
+  <ButtonCounter />
+  <ButtonCounter />
+  <ButtonCounter />
+  <ButtonCounter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonCounter from './components/ButtonCounter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ButtonCounter
   }
 }
 </script>
